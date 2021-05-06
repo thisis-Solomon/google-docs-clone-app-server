b02385a2b4e9eddf47e68f91b165c0f89702fb27
thisis-Solomon <solomonnjobvu1@gmail.com>
2021-05-06 19:15:51 +0200

    with a local hosthost mongodb

---
+++ Document.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const documentSchema = new Schema({
+    _id: String,
+    data: Object,
+});
+
+module.exports = model("Document", documentSchema);
